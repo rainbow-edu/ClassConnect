ceb7e921b6ce11adc44f1d54ba3fb7a0123f9eb7
rainbow-edu <s1106236@taivs.tp.edu.tw>
2024-10-10 14:15:55 +0800

    Client: Add theme

--- composeApp/src/commonMain/kotlin/tw/edu/tp/taivs/classconnect/App.kt
+++ composeApp/src/commonMain/kotlin/tw/edu/tp/taivs/classconnect/App.kt
@@ -2,8 +2,11 @@ package tw.edu.tp.taivs.classconnect
 
 import androidx.compose.runtime.Composable
 import tw.edu.tp.taivs.classconnect.ui.Navigation
+import tw.edu.tp.taivs.classconnect.ui.theme.AppTheme
 
 @Composable
 fun App() {
-    Navigation()
+    AppTheme {
+        Navigation()
+    }
 }
---
+++ composeApp/src/commonMain/kotlin/tw/edu/tp/taivs/classconnect/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package tw.edu.tp.taivs.classconnect.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
